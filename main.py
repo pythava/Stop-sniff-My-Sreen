3a2dc87228e91f6c07c8f144ed68e0c457bc2864
pythava <pythava1103@gmail.com>
2026-09-08 21:47:12 +0900

    로그 기능 추가중

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from plyer import notification
 from insightface.app import FaceAnalysis
 import time
+from datetime import datetime
 
 def notice(title, message, app_name, timeout):
     notification.notify(
@@ -13,19 +14,38 @@ def notice(title, message, app_name, timeout):
         timeout=timeout
     )
 
-with open('owner.pkl', 'rb') as f:
-    owner_embedding = pickle.load(f)
+# 이거 로그기능인데 언젠가 사용하기
+def logs(user, message, start_time, end_time):
+    with open(f'screen{now.date() + now.time()}.log', 'w', encoding='utf-8') as f:
+        text = f"[{user}]\t"
+        text += message + "\t"
+        text += str(start_time) 
+        text += " ~ "
+        text += str(end_time) + "\n"
+        f.write(text)
+    
+try:
+    with open('owner.pkl', 'rb') as f:
+        owner_embedding = pickle.load(f)
+except FileNotFoundError as e:
+    print(f"[Error] file open error : {e}")
+    exit()
 
 app = FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
 app.prepare(ctx_id=0, det_size=(320, 320))
 
 flag = 0
+detector = ""
+detection_time = 0
 cap = cv2.VideoCapture(0)
 
 THRESHOLD = 0.45
 FLAG_time = 30
 last_time = 0
 return_time = 10
+
+now = datetime.now()
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -43,8 +63,10 @@ while cap.isOpened():
 
         bbox = face.bbox.astype(int)
         if similarity >= THRESHOLD:
+                
             text = f'Owner detected ({similarity:.2f})'
             color = (0, 255, 0)
+            f.write
         else:
             if last_time != 0:
                 print(11)
